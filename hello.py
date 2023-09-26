c2fec40b653b65a689be51e43c2a72a4c430ab77
Test_C <1772600412@qq.com>
2023-09-26 11:11:03 +0800

    添加函数

--- hello.py
+++ hello.py
@@ -2,8 +2,10 @@ print('nihao')
 print('nihao')
 print('nihao')
 print('nihao')
-print('nihao')
-print('nihao')
-print('nihao')
 for i in range(10):
     print(i)
+
+def test():
+    print(123)
+
+test()
